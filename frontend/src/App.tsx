86d41f5c5c1cef4e1d831d982d9b798a5eb1b262
Dmitry G <sdprecovery@gmail.com>
2022-10-24 11:17:14 +0300

    update: frontend/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
 export default function App() {
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+  const { sendJsonMessage } = useWebSocket("ws://127.0.0.1:8000/");
   const { readyState } = useWebSocket("ws://127.0.0.1:8000/", {
     onOpen: () => {
       console.log("Connected!");
     },
     onClose: () => {
       console.log("Disconnected!");
+    },
+    onMessage: (e) => {
+      const data = JSON.parse(e.data);
+      switch (data.type) {
+        case "welcome_message":
+          setWelcomeMessage(data.message);
+          break;
+        default:
+          console.error("Unknown message type!");
+          break;
+      }
     }
   });
 
@@ -22,6 +35,18 @@ export default function App() {
   return (
     <div>
       <span>The WebSocket is currently {connectionStatus}</span>
+      <p>{welcomeMessage}</p>
+      <button
+          className="bg-gray-300 px-3 py-1"
+          onClick={() => {
+            sendJsonMessage({
+              type: "greeting",
+              message: "Hi!"
+            });
+          }}
+        >
+        Say Hi
+      </button>
     </div>
   );
 }
